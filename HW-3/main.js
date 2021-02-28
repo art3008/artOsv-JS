416bcdbf8b4b8164a1330efd1036bbe4cee9e047
art3008 <art.osv@mail.ru>
2021-02-28 22:13:01 +0600

    HW-3

---
+++ HW-3/main.js
@@ -0,0 +1,36 @@
+const loggerApple = (balance) =>{
+
+    console.log("------------")
+    console.log("В ящике " + balance + " яблок");
+    if(balance > 0){
+        console.log("Ящик не пустой");
+    }else if(balance === 0){
+        console.log("Ящик пустой");
+    }else{
+        console.log("Вы должны клиенту яблок");
+    }
+
+}
+
+const loggerTrans = amount =>{
+    if(amount >= 0 ){
+        console.log("Вы получили " + amount + " яблок");
+    }else{
+        console.log("Вы потеряли " + amount + " яблок");
+    }
+}
+
+const randomInt = (min,max) =>
+    min + Math.floor(Math.random() * (max - min));
+
+let balance = 0;
+let amount = 0;
+let sum = 0;
+
+while(balance <= 50){
+    
+    loggerApple(balance);
+    amount += randomInt(-5,10);
+    balance += amount;
+    loggerTrans(amount);
+}
