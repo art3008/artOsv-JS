5d1fb02d2fca96d79cdadecba3fc98ee11051088
art3008 <art.osv@mail.ru>
2021-03-04 19:19:27 +0600

    Готово

--- HW-5/main.js
+++ HW-5/main.js
@@ -52,7 +52,6 @@ const createSumArray = (array) =>{
 
 const averageValue = (array) =>{
 
-    let sum = 0;
     let result = 0;
     for (let i = 0; i < array.length; i++) {
         result =  Math.floor((sumArray(array))/array.length);
