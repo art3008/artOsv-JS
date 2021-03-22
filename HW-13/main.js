2514ddcc469a43994d4af9fa6f74d7438b91e26b
art3008 <art.osv@mail.ru>
2021-03-22 23:13:59 +0600

    Проверка на необезательное поле не добавил

---
+++ HW-13/main.js
@@ -0,0 +1,91 @@
+let validForm = {
+    username: "anton",
+    password: "anton!anton",
+    age: "20",
+    growth: "150",
+  };
+  
+  let invalidForm = {
+    username: "anto",
+    password: "anto",
+    age: "20a",
+    growth: "315.1"
+  };
+  
+  // const maxUsernameLength = 5;
+  
+  const handleSubmit = (form) => {
+    // const username = form.username;
+    // ..
+    const { username, password, age,growth } = form;
+  
+    let isValid = true;
+    let errors = { };
+  
+    if (username.length < 5) {
+      errors["username"] = "Логин должен быть не меньше 5 символов";
+      isValid = false;
+    }
+    
+    if (password.length < 10) {
+      errors["password"] = "Пароль должен быть не меньше 8 символов";
+      isValid = false;
+    }
+  
+    if (password.indexOf("!") < 0) {
+      errors["password"] = "Пароль должен содержать '!'";
+      isValid = false;
+    }
+  
+    let ageNumber = parseInt(age);
+    // "20" !== "20.1"
+     
+    
+    if (isNaN(ageNumber)  || age !== String(ageNumber)) {
+      errors["age"] = "Возраст должен быть числом";
+      isValid = false;
+    }
+  
+    if (age < 0 || age > 150) {
+      errors["age"] = "Возраст должен быть в промежутке от 0 до 150 лет";
+      isValid = false;
+    }
+
+    let growthValue = parseInt(growth);
+
+    if(growth < 0 || growth > 300){
+        errors["growth"] = "Рост должен быть в промежутке от 0 до 300";
+        isValid = false;
+    }
+  
+    if(Number.isInteger(growthValue)){
+        errors["growth"] = "Знаячение роста должно быть целым";
+        isValid = false;
+    }
+
+    if (isValid) {
+      // отправить форму 
+      const user = {
+        username,
+        password,
+        age,
+        growth,
+        dateOfRegistration: new Date(),
+      };
+  
+      console.log("Создаем пользователя", user);
+    } else {
+      // отображаем ошибки в форме
+      console.log("Форма заполнена с ошибками", errors, form);
+    }
+  }
+  
+  /// 
+  
+  handleSubmit(validForm);
+  console.log("-----");
+  handleSubmit(invalidForm);
+//   В процедурной версии добавить проверку поле рост
+// необязателньое
+// целое число +
+// от 0 до 300
