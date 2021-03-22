02d97da5a32ccf1f176e1a60773ecb00a6ed49ff
art3008 <art.osv@mail.ru>
2021-03-22 21:13:26 +0600

    ddd

--- HW-10/main.js
+++ HW-10/main.js
@@ -1,71 +1,44 @@
-const logRectangle = rectangle => 
-  console.log("Прямоугольник: " + rectangle.width + ", " + rectangle.height);
-
-const logTriangle = triangle =>
-    console.log("Треугольник: " + triangle.sideA + ", " + triangle.sideB + ", " + triangle.sideC)
+const logRectangle = rectangle =>
+    console.log("Прямоугольник: " + rectangle.width + "," + rectangle.height);
 
 const logCircle = circle =>
-    console.log("Круг " + circle.radius);
+    console.log("Круг: " + circle.radius);
+
+const logTriangle = triangle =>
+    console.log("Треугольник: " + triangle.a + "," + triangle.b + "," + triangle.c);
+
+const constructorRectangle = (width,height) => ({
+    width,
+    height,
+    log: function() {
+        logRectangle(this)
+    }
+
+});
+
+const constructorCircle = (radius) => ({
+    radius,
+    log: function() {
+        logCircle(this)
+    }
+});
+
+const constructorTriangle = (a,b,c) => ({
+    a,
+    b,
+    c,
+    log: function() {
+        logTriangle(this)
+    }
+});
 
 
-function Circle (radius) {
-    this.radius = radius;
-}
-
-Circle.prototype.$type = "circle";
-
-Circle.prototype.square = function(){
-    return Math.PI * this.radius * 2;
-}
-
-Circle.prototype.log = function() {
-    logCircle(this);
-}
+const rectangle1 = constructorRectangle(100,150);
+const circle1 = constructorCircle(100);
+const triangle1 = constructorTriangle(100,150,300);
 
 
+const shapes = [rectangle1,circle1,triangle1];
 
-function Triangle (sideA,sideB,sideC) {
+shapes.forEach(shape => shape.log(shape))
 
-    this.sideA = sideA;
-    this.sideB = sideB;
-    this.sideC = sideC;
-}
-
-Triangle.prototype.$type = "triangle";
-
-Triangle.prototype.square = function() {
-    let polup = (this.sideA + this.sideB + this.sideC)/2;
-    
-    return "Площадь " +  (Math.sqrt(polup * (polup - this.sideA) * (polup - this.sideB) * (polup - this.sideC)));
-}
-
-Triangle.prototype.log = function(){
-    logTriangle(this);
-}
-
-
-function Rectangle (width,height) {
-
-    this.width = width;
-    this.height = height;
-}
-
-Rectangle.prototype.$type = "rectangle";
-
-Rectangle.prototype.square = function() {
-    return (this.width * this.height);
-}
-
-Rectangle.prototype.log = function() {
-    logRectangle(this);
-}
-
-const shapes = [
-    new Rectangle(100,50),
-    new Triangle(2,3,4),
-    new Circle(2)
-];
-
-console.log(shapes);
-shapes.forEach(shape => shape.log());
-console.log(shapes.map(shape => shape.square()));
