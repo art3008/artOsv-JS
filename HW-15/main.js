b7e1a130b1f1580e813712bef09ba1fe2297da20
art3008 <art.osv@mail.ru>
2021-03-25 19:15:52 +0600

    Готово

---
+++ HW-15/main.js
@@ -0,0 +1,10 @@
+const reverseStringRecursive = string => {
+    if (string.length === 1) { 
+      return string;
+    } 
+    else {
+      return reverseStringRecursive(string.slice(1)) + string[0];
+    }
+  }
+  
+console.log(reverseStringRecursive("Привет"));
