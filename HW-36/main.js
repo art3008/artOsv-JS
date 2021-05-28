3f30c0722b52dfa2eab5ce31106bd758e156b89f
art3008 <art.osv@mail.ru>
2021-05-28 10:43:55 +0600

    HW-36

--- HW-36/main.js
+++ HW-36/main.js
@@ -1,130 +1,65 @@
-console.log(window.indexedDB);
+const $fetch = async (url, method = "GET", body = null) => {
+  const response = await fetch(url, {
+    method,
+    body: body //? JSON.stringify(body) : null
+  }); 
 
-const request = window.indexedDB.open("books", 5);
-
-/**
- * @type {IDBDatabase}
-*/
-
-
-let db = null;
-
-request.addEventListener("error", (e) => {
-  console.log(request, e);
-});
-
-request.addEventListener("success", (e) => {
-  db = request.result;
-
-  db.addEventListener("error", e => {
-    console.log("Error", e);
-  });
-
-  const transaction = db.transaction("books", "readwrite");
-
-  transaction.addEventListener("complete", e => {
-    console.log("Cохранили", e);
-    
-    f();
-  });
-
-  const store = transaction.objectStore("books");
-
-  store.put({
-    id: 1,
-    title: "ASDfasdf",
-    author: "asdfsadf",
-    date: new Date(),
-    tags: ["asdf", "zcxv", "123"]
-  });
-
-  store.put({
-    id: 2,
-    title: "ASDfa123412sdf",
-    date: new Date(),
-    tags: ["asdf", "asdf", "zcxv", "123"],
-    isbn: "0001-234-234"
-  });
-
-  store.put({
-    id: 3,
-    title: "ASsadfsadfDfa123412sdf",
-    author: "as sdaf asdfsadf",
-    date: new Date(),
-    tags: ["asdfasdf", "asdf", "zcxv", "123"],
-    isbn: "0001-234-234sadf"
-  });
-
-  store.put({
-    id: 4,
-    title: "Жюль Верн",
-    author: "Таинственный остров",
-    date: new Date(),
-    tags: ["asdfasdf", "asdf", "zcxv", "123"],
-    isbn: "0001-234-234sadf"
-  });
-
-  console.log(db, e);
-
-});
-
-request.addEventListener("upgradeneeded", (e) => {
-  console.log(request, e);
-  
-  const db = request.result;
-
-  switch (e.oldVersion) {
-    case 0: 
-    case 1: 
-    case 2:
-    case 3: {
-      const store = db.createObjectStore("books", {
-        keyPath: "id"
-      });
-    }
-
-    case 4: {
-      const store = e.target.transaction.objectStore("books");
-      store.createIndex("author", "author");
-    }
-
+  if (response.ok) {
+    const result = await response.json();
+    return result;
+  } else {
+    return response.status + " " + response.statusText;
   }
+}
 
-  // request.transaction.abort();
-});
+window.addEventListener("load", async () => {
 
+  const form = document.forms["test"];
 
+  const inputFile = form.elements["file"];
 
-const printResult = e => {
-  console.log(e.target.result);
-};
+  let fileQueue = [];
 
-const printResult2 = e => {
-  console.log(e.target.result, e);
-};
-
-
-const f = () => {
-  
-  const transaction = db.transaction("books", "readonly");
-
-  transaction.addEventListener("complete", e => {
-    console.log("OK");
+  inputFile.addEventListener("change", e => {
+    fileQueue = fileQueue.concat(Array.from(inputFile.files));
   });
 
-  const store = transaction.objectStore("books");
+  form.addEventListener("submit", async e => {
+    e.preventDefault();
+    const data = new FormData(form);
+    data.append("extra", 123);
+    data.delete("file");
 
-  // store.count().onsuccess = printResult;
+    fileQueue.forEach((file, index) => {
+      data.append("file[" + index + "]", file, "file" + index + ".jpg");
+    });
+
+
+    // console.log(fileQueue);
+
+    console.log(
+      await $fetch("test.json", "POST", 
+        data
+      )
+    );  
+  })
+
+  // const form = new FormData();
+  // form.append("name", "myname");
+  // console.log(
+  //   await $fetch("test.json", "POST", 
+  //     form
+  //   )
+  // );
+    
+  // console.log(
+  //   await $fetch("test2.json")
+  // );
   
-  const keyRange = IDBKeyRange.bound(2, 3, false, false);  
-  store.getAll(keyRange).onsuccess = printResult;
+})
 
   
-
-  const authorRange = IDBKeyRange.lowerBound("asa", true);
-
-  const index = store.index("author");
-  index.getAll(authorRange).onsuccess = printResult;
-  index.get(authorRange).onsuccess = printResult;
-
-}
+// console.log(
+//   fetch("https://logbook.itstep.org/")
+//   .then(response => console.log(response))
+//   );
