2075f600d5e2853709bc2539a4c090b60ebada47
art3008 <art.osv@mail.ru>
2021-07-08 16:00:43 +0600

    HW-27 done

--- HW-27/progress.js
+++ HW-27/progress.js
@@ -1,45 +1,71 @@
-
 const mock = () => {
   let sections = createArray(index => [
     $("h1", {}, "Header " + (index + 1)),
     $("section", {
       style: {
-        height: randomInt(500, 2000) + "px"
+        height: randomInt(200, 1000) + "px"
       }
-    })
-  ]) (7);
+    }, $("h2", {}, "Second header of header " + (index + 1)))
+  ])(7);
 
   document.body.append(...sections.flat());
 
 };
 
-
 window.addEventListener("load", () => {
 
-  mock();
-
+  const documentWidth = document.body.offsetWidth;
   const documentHeight = document.body.offsetHeight - window.innerHeight;
 
   const headerPositions = Array.from(document.querySelectorAll("h1")).map(element => element.offsetTop);
 
-  console.log(headerPositions);
+  const headerContents = Array.from(document.querySelectorAll("h1")).map(element => element.innerText);
 
-  const checkpoints = headerPositions.map(position => 
-    $("div", { 
-        className: "progress-bar__checkpoint",
-        style: {
-          left: (position * 100 / documentHeight) + "%"
-        },
-        onclick: () => {
-          // window.scrollBy - прокручивает НА Y пикселей от текущей позиции скорла 
-          window.scrollTo({
-            left: 0,
-            top: position,
-            behavior: "smooth"
-          });
-        }
-      })
+  const subtitlePositions = Array.from(document.querySelectorAll("h2")).map(element => element.offsetTop);
+  subtitlePositions.forEach(pos => console.log((pos * 100 / documentHeight)));
+  const subtitleContents = Array.from(document.querySelectorAll("h2")).map(element => element.innerText);
+
+  console.log(headerPositions, headerContents);
+
+  const checkpoints = headerPositions.map(position =>
+    $("div", {
+      className: "progress-bar__checkpoint",
+      style: {
+        left: (position * 100 / documentHeight) + .5 + "%"
+      },
+      onclick: () => {
+        window.scrollTo({
+          left: 0,
+          top: position,
+          behavior: "smooth"
+        });
+      }
+    })
   );
+
+  const subtitleCheckpoints = subtitlePositions.map(position =>
+    $("div", {
+      className: "progress-bar__checkpoint subtitle_checkpoint",
+      style: {
+        left: getCords(position, documentHeight)
+      },
+      onclick: () => {
+        window.scrollTo({
+          left: 0,
+          top: position,
+          behavior: "smooth"
+        });
+      }
+    })
+  );
+
+  let index = 0;
+  checkpoints.forEach(checkpoint => {
+    checkpoint.append($("div", { className: "checkpoint__tooltip" }, $("span", {}, headerContents[index])));
+
+    index++;
+  });
+  index = 0;
 
   const progressBar = $("div", {
     className: "progress-bar__bar"
@@ -47,12 +73,38 @@ window.addEventListener("load", () => {
 
   const progressContainer = $("div", {
     className: "progress-bar"
-  }, progressBar, ...checkpoints);
+  }, progressBar, ...checkpoints, ...subtitleCheckpoints);
+
+
 
   document.body.append(progressContainer);
 
+  subtitleCheckpoints.forEach(checkpoint => {
+    checkpoint.append($("div", { className: "checkpoint__tooltip" }, $("span", {}, subtitleContents[index])));
+    if (checkpoint.offsetLeft > documentWidth / 2) {
+      let tooltips = checkpoint.children;
+      for (let i = 0; i < tooltips.length; i++) {
+        tooltips[i].classList.add("tooltip_left_direction");
+
+      }
+    }
+    index++;
+  });
+  index = 0;
+  checkpoints.forEach(checkpoint => {
+    checkpoint.append($("div", { className: "checkpoint__tooltip" }, $("span", {}, headerContents[index])));
+    if (checkpoint.offsetLeft > documentWidth / 2) {
+      let tooltips = checkpoint.children;
+      for (let i = 0; i < tooltips.length; i++) {
+        tooltips[i].classList.add("tooltip_left_direction");
+
+      }
+    }
+    index++;
+  });
+
   const throttledHandler = throttle(() => {
-    
+
     let lastIndex = headerPositions.findIndex(position => window.scrollY < position);
     if (lastIndex < 0) {
       lastIndex = checkpoints.length;
@@ -66,17 +118,20 @@ window.addEventListener("load", () => {
         checkpoint.classList.remove("progress-bar__checkpoint_active");
       }
     })
-
-
     console.log(window.scrollY, document.body.offsetHeight - window.innerHeight);
 
     progressBar.style.width = (window.scrollY * 100 / (document.body.offsetHeight - window.innerHeight)) + "%";
 
-
-    
-  }) (100);
+  })(100);
 
   window.addEventListener("scroll", throttledHandler);
 
-
 });
+
+const getCords = (position, documentHeight) => {
+  let pos = (position * 100 / documentHeight);
+  if (pos > 100) {
+    pos = 99;
+  }
+  return pos + .5 + "%";
+}
